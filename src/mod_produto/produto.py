2ad5bd52da255a02a50d489bc7128b7c43069289
MarcosVinicius556 <marcosvinicios4132@gmail.com>
2024-05-07 16:29:38 -0300

    Listagem, criação, edição e deleção criados para o produto

--- src/mod_produto/produto.py
+++ src/mod_produto/produto.py
@@ -3,6 +3,10 @@ from flask import Blueprint, render_template
 bp_produto = Blueprint('produto', __name__, url_prefix="/produto", template_folder='templates')
 
 ''' rotas dos formulários '''
-@bp_produto.route('/')
+@bp_produto.route('/', methods=["GET", "POST"])
 def formListaProduto():
     return render_template('formListaProduto.html'), 200
+
+@bp_produto.route('/new')
+def formNovoProduto():
+    return render_template('formNovoProduto.html'), 200
