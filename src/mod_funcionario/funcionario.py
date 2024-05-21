0f866e28dadeb620e614947d1ea69ee5bf652f20
MarcosVinicius556 <marcosvinicios4132@gmail.com>
2024-05-21 14:52:20 -0300

    Downgrade da versão do bcrypt...

--- src/mod_funcionario/funcionario.py
+++ src/mod_funcionario/funcionario.py
@@ -3,6 +3,8 @@ import requests
 # from mod_login.login import validaLogin
 from settings import getHeadersAPI, ENDPOINT_FUNCIONARIO
 
+from funcoes import Funcoes;
+
 bp_funcionario = Blueprint('funcionario', __name__, url_prefix="/funcionario", template_folder='templates')
 
 ''' rotas dos formulários '''
@@ -24,3 +26,32 @@ def formListaFuncionario():
 @bp_funcionario.route('/new', methods=["GET"])
 def formNovoFuncionario():
     return render_template('formNovoFuncionario.html'), 200
+
+@bp_funcionario.route('/insert', methods=['POST'])
+def insert():
+    try:
+        # dados enviados via FORM
+        id_funcionario = request.form['codigo']
+        nome = request.form['nome']
+        matricula = request.form['matricula']
+        cpf = request.form['cpf']
+        telefone = request.form['telefone']
+        grupo = request.form['grupo']
+        print(request.form)
+        senha = Funcoes.get_password_hash(request.form['senha'])
+        
+        # monta o JSON para envio a API
+        payload = {'nome': nome, 'matricula': matricula, 'cpf': cpf, 'telefone': telefone, 'grupo': grupo, 'senha': senha}
+        
+        # executa o verbo POST da API e armazena seu retorno
+        response = requests.post(ENDPOINT_FUNCIONARIO, headers=getHeadersAPI(), json=payload)
+        result = response.json()
+       
+        if (response.status_code != 200 or result[1] != 200):
+            raise Exception(result)
+            
+        return redirect(url_for('funcionario.formListaFuncionario', msg=result[0]))
+        
+    except Exception as e:
+        print(e);
+        return render_template('formListaFuncionario.html', msgErro=e.args[0])
